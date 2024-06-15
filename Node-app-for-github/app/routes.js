2e837154371cbc71d7921e5e7cfbeedd6cc003e2
Sujit Ghodajkar <45931379+sujit-ghodajkar@users.noreply.github.com>
2024-06-15 20:00:36 +0530

    Delete Node-app-for-github directory
    doing correction

--- Node-app-for-github/app/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-    app.get('/', function(req, res) { // CODE BLOCK RESPONDING TO THE CLIENT'S '/' REQUEST
-        res.render('index'); // INDEX VIEW FILE RENDERED
-    });
-
-}
